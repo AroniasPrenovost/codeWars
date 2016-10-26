758e6fd005cbb79bd867e0a44a44126ee115895f
AroniasPrenovost <Aroniascoder@gmail.com>
2016-10-26 07:54:02 -0700

    first commit

---
+++ javaScript-Kata/BinaryAddition.js
@@ -0,0 +1,22 @@
+// BINARY ADDITION 
+
+// Implement a function that adds two numbers together 
+// and returns their sum in binary. The conversion can 
+// be done before, or after the addition. 
+// The binary number returned should be a string.
+
+// My Answer
+
+function addBinary(a,b) {
+//takes value of 2 integers
+var numberOne = a + b;
+//takes integer - gets base numeric value
+return (numberOne >>> 0).toString(2);
+}
+
+
+// refactored further 
+
+function addBinary(a,b){
+  return ((a + b) >>> 0).toString(2);
+}
