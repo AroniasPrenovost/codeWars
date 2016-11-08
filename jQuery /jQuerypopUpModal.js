1af38161151373d9c3772d0bb2112ecfc42afa72
AroniasPrenovost <Aroniascoder@gmail.com>
2016-11-07 23:35:30 -0800

    added jquery popup modal functionality

---
+++ jQuery /jQuerypopUpModal.js
@@ -0,0 +1,38 @@
+// javascript popup modal using jQuery
+
+
+<!-- HTML -->
+
+<button id="modal">click here</button>
+
+<!-- CSS -->
+
+.popup {
+  width: 200px;
+  height:200px;
+  background-color: red;
+}
+
+<!-- jQuery -->
+
+// attaches a click event to id and calls function  
+$("#modal").click(function() {
+
+// grabs the DOM element at the index 
+    if ($(".popup")[0]) {
+      
+// removes DOM element - adds "click again to close" functionality
+        $('.popup').remove();
+      
+// calls designated class that only lives in CSS and generates new button
+    } else {
+        $("#modal").parent().append('<div class="popup"><button id="modalButton">close</button></div>');
+    }
+});
+
+// adds button functionality to 
+$(document).on('click', "#modalButton", function() {
+
+// remove Dom element
+    $(".popup").remove();
+});
