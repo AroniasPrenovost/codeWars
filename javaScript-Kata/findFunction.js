d974ee5c7870dac73e768a86f2721b5784d82c1d
AroniasPrenovost <Aroniascoder@gmail.com>
2016-12-13 18:36:21 -0800

    refactord js problem

--- javaScript-Kata/findFunction.js
+++ javaScript-Kata/findFunction.js
@@ -23,11 +23,46 @@ return Four.split('.')[0];
   } else if (url.includes("www.")) {
 return Five.split('.')[0];
   } else  {
-  	return Six.split('.')[0];;
+  	return Six.split('.')[0];
   }
 }
 
 domainName("https://thisisatesturl.com");
+
+
+
+
+// refactored 
+
+
+
+function domainName(url){
+  if (url.includes("s:\//www")) {
+return url.slice(12).split('.')[0]; 
+   } else if (url.includes("p:\//www")) {
+return url.slice(11).split('.')[0];
+  } else if (url.includes("ps:")) {
+return url.slice(8).split('.')[0];
+  } else if (url.includes("ttp:")) {
+return url.slice(7).split('.')[0];
+  } else if (url.includes("www.")) {
+return url.slice(4).split('.')[0];
+  } else  {
+  	return url.slice(0).split('.')[0];
+  }
+}
+
+domainName("https://thisisatesturl.com");
+
+
+
+
+
+
+
+
+
+
 
 
 
