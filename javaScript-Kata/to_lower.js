ef9d528b38265a4e64c033ad486abc4c2d3e03dc
AroniasPrenovost <Aroniascoder@gmail.com>
2016-12-22 14:03:46 -0800

    finished Pascal case to underscore case function

--- javaScript-Kata/to_lower.js
+++ javaScript-Kata/to_lower.js
@@ -1,17 +1,6 @@
 function toUnderscore(string) {
-
-// needs if/else for number vs. string 
-
-string = string.replace(/([a-z])([A-Z])/g, '$1_$2');
-// adds _ after letter + number combo 
-string = string.replace(/([0-9])(?=[a-z])/ig, '$1_');
-
-    return string.toLowerCase();
-
+  return (string+'').replace(/([^A-Z])([A-Z])/g, "$1_$2").toLowerCase();
 }
-
-toUnderscore("ThisIsSick");
-
 
 
 
