c1db5fd29323c4bfa7cf773c199bae645d4c6269
AroniasPrenovost <Aroniascoder@gmail.com>
2016-12-31 11:07:53 -0800

    solved caesar cipher problem

---
+++ javaScript-Kata/rot13.js
@@ -0,0 +1,28 @@
+function rot13(message) {
+
+  var output = '';
+  for (var i = 0; i < message.length; i ++) {
+    var c = message[i];
+    if (c.match(/[a-z]/i)) {
+      var code = message.charCodeAt(i);
+      if ((code >= 65) && (code <= 90))
+        c = String.fromCharCode(((code - 65 + 13) % 26) + 65);
+      else if ((code >= 97) && (code <= 122))
+        c = String.fromCharCode(((code - 97 + 13) % 26) + 97);
+    }
+    output += c;
+  }
+  return output;
+};
+
+rot13("testtt");
+
+
+
+
+
+
+
+
+
+
