e265a09f021fa9bce54cc8baf728fafa47dcdda2
AroniasPrenovost <Aroniascoder@gmail.com>
2017-03-11 16:46:45 -0800

    still debugging

--- javaScript-Kata/stripUrlParams.js
+++ javaScript-Kata/stripUrlParams.js
@@ -7,7 +7,7 @@
 function stripUrlParams(url, paramsToStrip){
 // complete me
 
-// checks if if has query string
+// if url contains query string
 if (url.split("?").length - 1 > 0) {
 
 // turns paramsToString to string
@@ -18,37 +18,49 @@ var qString = url.substring(url.indexOf(".com") + 4);
 // make qString 8 characters max - double checks
 var length = 8;
 var trimmedString = qString.substring(0, length);
-//  return trimmedString;
 
 // removes paramsToStrip matches
-// BUT it's a WIP since need to remove that +2 spaces
 var x = trimmedString.split(pString).join('');
 // return x;
-
-var second = x.charAt(5);
-var first = x.charAt(1);
-
-// if the 6th spot of the array = "=", slice after the &
-if (first === "=") {
-	var bindex = 1; var eindex = 4; 
-	var finalq = x.slice(0, bindex) + x.slice(eindex);
-	return finalq;
-// if first area is a match
-} else if (second === "=") {
-	var finalq = x.split('&')[0];
-	return finalq;
-} else {
-	var finalq = x;
-	return finalq;
-}
-
-} else {
-//	return url;
-}
 
 // removes query string
 var plainUrl = url.substring(0, url.indexOf("?"));
 
 
+
+var first = x.charAt(1);
+var second = x.charAt(4);
+
+
+// return "yes " + hasDuplicates;
+// if querystring contains pString happens more than once 
+
+
+// if both variables match parameters, don't return query string 
+ if (first && second === "=") {
+
+ var finalq = trimmedString.slice(0, 9);
+	return plainUrl + finalq;
+} else if (first === "=") {
+	var bindex = 1; var eindex = 4;
+	var finalq = x.slice(0, bindex) + x.slice(eindex);
+	return plainUrl + finalq;
+// if first area is a match
+} else if (second === "=") {
+	var finalq = x.split('&')[0];
+	return plainUrl + finalq;
+} else {
+	var finalq = x;
+	return plainUrl + finalq;
 }
-stripUrlParams("www.chat.com?a=1&b=1", ["a"]);
+
+// if ir doesn't contain query string
+} else {
+	return url;
+}
+
+}
+stripUrlParams("www.chat.com?a=1&a=1", ["a"]);
+
+// still needs to account for a query string that look slike "?a=1&a=2"
+
