8f8c1a591031a387457d84a56cb70a0bc1ebd475
AroniasPrenovost <Aroniascoder@gmail.com>
2017-03-26 10:50:00 -0700

    solved problem, but need to remove hardcoded parameters and add string to acronym feature

--- javaScript-Kata/breadCrumb.js
+++ javaScript-Kata/breadCrumb.js
@@ -8,68 +8,36 @@ var lengthn = urlString.split("/").length-1;
 var arrayElements = urlString.split(/[/\/]/).filter(Boolean);
 // assign and remove span value from array 
 var spanE = arrayElements.splice(-1)[0];
+
 // length of array after span value is taken
 var aLength = arrayElements.length;
-
 var one = arrayElements[0];
-
 var two = arrayElements[1];
-
 var three = arrayElements[2];
-
 var four = arrayElements[3];
-
-var spanEe = "<span class=\"active" + one + "/" + two + "/" + three + "/" + four + "/>\"</a>"
 var finalString = ["<a href=\"/\">HOME</a>"];
-var finalNotString = [];
 
 if (one !== undefined) {
 var onee = "<a href=\"/" + one + "/\">" + one.toUpperCase() + "</a>";  // follow this example for formatting 
 finalString.push(onee);
 } 
-
 if (two !== undefined) {
-	var twoo = "<a href=\"/" + one + "/" + two + "/\>" + two.toUpperCase() + "</a>"
+	var twoo = "<a href=\"/" + one + "/" + two + "/\">" + two.toUpperCase() + "</a>"
 	finalString.push(twoo);
 }
-
 if (three !== undefined) {
-	var threee = "<a href=\"/" + one + "/" + two + "/" + three + "/\>" + three.toUpperCase() + "</a>"
+	var threee = "<a href=\"/" + one + "/" + two + "/" + three + "/\">" + three.toUpperCase() + "</a>"
 	finalString.push(threee);
 }
-
 if (four !== undefined) {
-	var fourr = "<a href=\"/" + one + "/" + two + "/" + three + "/" + four + "/\>" + four.toUpperCase() + "</a>"
+	var fourr = "<a href=\"/" + one + "/" + two + "/" + three + "/" + four + "/\">" + four.toUpperCase() + "</a>"
 	finalString.push(fourr);
 }
-
-
-return finalString;
-/*
-
-var counter = 0;
-while (aLength >= counter) { 
-// somethign goes here 
-
-++counter; // repeat the process if it is still not a palindrome 
+var spanEe = "<span class=\"active" + "\">" + spanE.toUpperCase() + "</a>"
+finalString.push(spanEe);
+return finalString.join().replace(/,/g , separator);
 }
-
-if (lengthn === 4) {
-
-} else if (lengthn === 3) {
-
-} else if (lengthn === 2) {
-
-} else {
-
-}
-*/
-/* 
-"<a href="\/">HOME</a> + " separator " + <a href="/ /"></a>
-+ " separator " + <span class="active"></span>"}
-*/
-}
-generateBC("mysite.com/pictures/holidays/tester/hi/index.html");
+generateBC("mysite.com/pictures/mysitestruff/holidays.html", " + ");
 
 
 
