ae1f9f96a3c31b37ea1a2e2e7a3eb3ae9e8a202e
AroniasPrenovost <Aroniascoder@gmail.com>
2017-04-04 23:28:45 -0700

    refactored js problem, need to find a way to sort elements by match

--- javaScript-Kata/directionReduction.js
+++ javaScript-Kata/directionReduction.js
@@ -1,22 +1,27 @@
 // Write a function dirReduc which will take an array of strings and 
 // returns an array of strings with the needless directions removed 
 // (W<->E or S<->N side by side).
-
 function dirReduc(arr){
+
+var directions = [];	
 
 // find # of "NORTH" occurences
 var countN = 0;
 for(var i = 0; i < arr.length; ++i){
     if(arr[i] == "NORTH")
         countN++;
-	} // return countN;
+	} 
+	if (countN === 1)
+   directions.push("NORTH"); 
 
 // find # of "SOUTH" occurences
 var countS = 0;
 for(var i = 0; i < arr.length; ++i){
     if(arr[i] == "SOUTH")
         countS++;
-	} // return countN;
+	}
+if (countS === 1)
+   directions.push("SOUTH"); 
 
 // find # of "EAST" occurences
 var countE = 0;
@@ -24,24 +29,37 @@ for(var i = 0; i < arr.length; ++i){
     if(arr[i] == "EAST")
         countE++;
 	} // return countN;
+	if (countE === 1)
+   directions.push("EAST"); 
 
 // find # of "WEST" occurences
 var countW = 0;
 for(var i = 0; i < arr.length; ++i){
     if(arr[i] == "WEST")
         countW++;
-	} // return countN;
+	} 
+	if (countW === 1)
+   directions.push("WEST"); 
 
-// returns empty string if all directions cancel out 
-if (countN == countS && countW == countE) {
-	return "";
-} else {
-// figure out what cancels what out 
-
-	}
+// returns empty string if all directions cancel out
+while (countN == countS && countW == countE) {
+	directions.push("");
+	return directions;
 }
+	return directions;
+}
+dirReduc(["NORTH", "NORTH", "SOUTH", "EAST", "EAST", "WEST", "WEST"]);
 
-dirReduc(["NORTH", "NORTH", "SOUTH", "SOUTH", "EAST", "EAST", "WEST", "WEST", "WEST"]);
+
+
+
+
+
+
+
+
+
+
 
 
 
