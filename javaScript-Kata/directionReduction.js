0f5bbfe2dfc7f77d4e221bfe5f1fb59847225b4b
AroniasPrenovost <Aroniascoder@gmail.com>
2017-04-05 22:20:33 -0700

    made progress on problem, just need to find a way to  restart iteration through array at beginning of array vs. AFTER the item is sliced

--- javaScript-Kata/directionReduction.js
+++ javaScript-Kata/directionReduction.js
@@ -4,7 +4,6 @@
 function dirReduc(arr){
 
 var directions = [];	
-
 // find # of "NORTH" occurences
 var countN = 0;
 for(var i = 0; i < arr.length; ++i){
@@ -12,7 +11,7 @@ for(var i = 0; i < arr.length; ++i){
         countN++;
 	} 
 	if (countN === 1)
-   directions.push("NORTH"); 
+   directions.push("NORTH");
 
 // find # of "SOUTH" occurences
 var countS = 0;
@@ -20,7 +19,7 @@ for(var i = 0; i < arr.length; ++i){
     if(arr[i] == "SOUTH")
         countS++;
 	}
-if (countS === 1)
+  if (countS === 1)
    directions.push("SOUTH"); 
 
 // find # of "EAST" occurences
@@ -46,14 +45,44 @@ while (countN == countS && countW == countE) {
 	directions.push("");
 	return directions;
 }
+ 
+
 	return directions;
 }
 dirReduc(["NORTH", "NORTH", "SOUTH", "EAST", "EAST", "WEST", "WEST"]);
 
 
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// Below may be a better approach
 
+function dirReduc(arr){
+	var directions = [];
+	var empty = [];
+	var countE = 0;
+	for(var i = 0; i < arr.length; ++i){
+		if(arr[i] == "NORTH") {
+			var arraycontainsSOUTH = (arr.indexOf("SOUTH") > -1);
+			if (arraycontainsSOUTH === true) {
+				var search_term = "SOUTH";
+				for (var i=arr.length-1; i>=0; i--) {
+					if (arr[i] === search_term) {
+						arr.splice(i, 1);
+var index = arr.indexOf("NORTH"); // removes first instance of "NORTH"
+arr.splice(index, 1);
+break;       // Uncomment  if only the first term has to be removed
+		}
+	}
+} else {
+empty.push(arr[i]); // storage for dead data 
+	} // below here is where we add new code
+} // the major for loop 
+countE++;    
+	} // closeing of for loop
+return arr; // this might be where that catch all thing is 
+}
+dirReduc(["NORTH", "NORTH", "SOUTH", "EAST", "EAST", "WEST", "WEST"]);
 
-
+// i need to make it so each iteration starts from beginning of array 
 
 
 
