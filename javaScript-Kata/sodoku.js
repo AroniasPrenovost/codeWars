bd10af782b63d29c7eb1b83fbd9ec9758a7a3c91
AroniasPrenovost <Aroniascoder@gmail.com>
2017-04-22 18:28:40 -0700

    hardcoding vertical column values, almost done with full prototype

--- javaScript-Kata/sodoku.js
+++ javaScript-Kata/sodoku.js
@@ -1,12 +1,13 @@
-// requirements
-// - check if every array is in a unique order
-// - they must all contain #'s 1-9
+// REQUIREMENTS
+ // - check if every array is in a unique order
+ // - they must all contain #'s 1-9
 function validSolution(board){
 	arrayVals = [];
 	for(var i = 0; i < board.length; i++){
 		for(var j = 0; j < board[i].length; j++){
 			arrayVals.push(board[i][j]);
 		}
+// ----------------------------------------------------
 // remove commas from each string and replace with +'s
 var regex = new RegExp(',', 'g');
 var row1 = arrayVals.slice(0, 9).toString().replace(regex, '+');
@@ -48,15 +49,28 @@ for (var i = 0; i < arrS.length + 1 - 1; i++) {
 	}
 }
 if (results.length > 0) {
-	return false;
+	alert(results);
 } else {
-	return true;
+	arrS = arrS;
 	}
+// -----------------------------------------------------
+// check if there are no duplicates in vertical indeces
+var vertOne = (eval(row1.charAt(0) + "+" + row2.charAt(0) + "+" + row3.charAt(0) + "+" + row4.charAt(0) + "+" + row5.charAt(0) + "+" + row6.charAt(0) + "+" + row7.charAt(0) + "+" + row8.charAt(0) + "+" + row9.charAt(0)));
+var vertTwo = (eval(row1.charAt(2) + "+" + row2.charAt(2) + "+" + row3.charAt(2) + "+" + row4.charAt(2) + "+" + row5.charAt(2) + "+" + row6.charAt(2) + "+" + row7.charAt(2) + "+" + row8.charAt(2) + "+" + row9.charAt(2)));
+var vertThree = (eval(row1.charAt(4) + "+" + row2.charAt(4) + "+" + row3.charAt(4) + "+" + row4.charAt(4) + "+" + row5.charAt(4) + "+" + row6.charAt(4) + "+" + row7.charAt(4) + "+" + row8.charAt(4) + "+" + row9.charAt(4)));
+var vertFour = (eval(row1.charAt(6) + "+" + row2.charAt(6) + "+" + row3.charAt(6) + "+" + row4.charAt(6) + "+" + row5.charAt(6) + "+" + row6.charAt(6) + "+" + row7.charAt(6) + "+" + row8.charAt(6) + "+" + row9.charAt(6)));
+var vertFive = (eval(row1.charAt(8) + "+" + row2.charAt(8) + "+" + row3.charAt(8) + "+" + row4.charAt(8) + "+" + row5.charAt(8) + "+" + row6.charAt(8) + "+" + row7.charAt(8) + "+" + row8.charAt(8) + "+" + row9.charAt(8)));
+var vertSix = (eval(row1.charAt(10) + "+" + row2.charAt(10) + "+" + row3.charAt(10) + "+" + row4.charAt(10) + "+" + row5.charAt(10) + "+" + row6.charAt(10) + "+" + row7.charAt(10) + "+" + row8.charAt(10) + "+" + row9.charAt(10)));
+var vertSeven = (eval(row1.charAt(12) + "+" + row2.charAt(12) + "+" + row3.charAt(12) + "+" + row4.charAt(12) + "+" + row5.charAt(12) + "+" + row6.charAt(12) + "+" + row7.charAt(12) + "+" + row8.charAt(12) + "+" + row9.charAt(12)));
+var vertEight = (eval(row1.charAt(14) + "+" + row2.charAt(14) + "+" + row3.charAt(14) + "+" + row4.charAt(14) + "+" + row5.charAt(14) + "+" + row6.charAt(14) + "+" + row7.charAt(14) + "+" + row8.charAt(14) + "+" + row9.charAt(14)));
+var vertNine = (eval(row1.charAt(16) + "+" + row2.charAt(16) + "+" + row3.charAt(16) + "+" + row4.charAt(16) + "+" + row5.charAt(16) + "+" + row6.charAt(16) + "+" + row7.charAt(16) + "+" + row8.charAt(16) + "+" + row9.charAt(16)));
+return vertOne + " " + vertTwo + " " + vertThree + " " + vertFour + " " + vertFive + " " + vertSix;
+
 }
 validSolution([[1,8,7,4,5,6,3,2,9], 
-	[1,8,7,4,6,5,3,2,9], 
+	[1,9,7,4,5,6,3,2,8], 
 	[1,2,3,4,6,5,7,8,9], 
-	[1,2,3,4,6,5,7,8,9], 
+	[1,2,7,4,6,5,3,8,9], 
 	[1,2,3,5,4,6,7,8,9], 
 	[1,2,4,3,5,6,7,8,9],
 	[1,3,2,4,5,6,7,8,9],
