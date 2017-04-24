104cbd19c3b4f88362ec927bbd639695caa6f414
AroniasPrenovost <Aroniascoder@gmail.com>
2017-04-23 19:51:48 -0700

    Lingering edge case I need to resolve, but 99% of sodoku solver functionality is there

--- javaScript-Kata/sodoku.js
+++ javaScript-Kata/sodoku.js
@@ -1,13 +1,21 @@
-// REQUIREMENTS
- // - check if every array is in a unique order
- // - they must all contain #'s 1-9
+//-- REQUIREMENTS --// 
+// - uniquely ordered arrays
+// - must #'s 1-9, no 0's 
 function validSolution(board){
-	arrayVals = [];
-	for(var i = 0; i < board.length; i++){
-		for(var j = 0; j < board[i].length; j++){
-			arrayVals.push(board[i][j]);
-		}
-// ----------------------------------------------------
+// return "false" if the table contains any 0's
+var stringboard = board.join("");
+var zeroCheck = stringboard.search("0");
+if (zeroCheck != -1) {
+	return false;
+} else {
+	board = board;
+}
+// if there are no 0's, let's turn the data into something more manageable
+var arrayVals = [];
+for(var i = 0; i < board.length; i++){
+	for(var j = 0; j < board[i].length; j++){
+		arrayVals.push(board[i][j]);
+	}
 // remove commas from each string and replace with +'s
 var regex = new RegExp(',', 'g');
 var row1 = arrayVals.slice(0, 9).toString().replace(regex, '+');
@@ -20,14 +28,13 @@ var row7 = arrayVals.slice(54, 63).toString().replace(regex, '+');
 var row8 = arrayVals.slice(63, 72).toString().replace(regex, '+');
 var row9 = arrayVals.slice(72, 81).toString().replace(regex, '+');
 } // end for loop
-// ----------------------------------------------------
 // evaluate the expression. numbers 1-9 will = 45
-rows = [eval(row1), eval(row2), eval(row3), eval(row4), eval(row5), eval(row6), eval(row7), eval(row8), eval(row9)];
+var rows = [eval(row1), eval(row2), eval(row3), eval(row4), eval(row5), eval(row6), eval(row7), eval(row8), eval(row9)];
 var wrong = [];
 var right = [];
 for(var i = 0; i < rows.length; i++){
 	if(rows[i] != 45) {
-		wrong.push(rows[i]);
+		return false;
 	} else {
 		right.push(rows[i]);
 	}
@@ -37,9 +44,8 @@ if (right.length > 8) {
 } else {
 	return false;
 }
-// ----------------------------------------------------
 // check if all arrays are in a different order
-arrS = [row1, row2, row3, row4, row5, row6, row7, row8, row9];
+var arrS = [row1, row2, row3, row4, row5, row6, row7, row8, row9];
 //  return rawRows;
 var sorted_arr = arrS.slice().sort();                        
 var results = [];
@@ -48,13 +54,13 @@ for (var i = 0; i < arrS.length + 1 - 1; i++) {
 		results.push(sorted_arr[i]);
 	}
 }
+
 if (results.length > 0) {
-	alert(results);
+	return false;
 } else {
 	arrS = arrS;
-	}
-// -----------------------------------------------------
-// check if there are no duplicates in vertical indeces
+}
+// if vertical "column" numbers = 9, they are also uniquely ordered!
 var vertOne = (eval(row1.charAt(0) + "+" + row2.charAt(0) + "+" + row3.charAt(0) + "+" + row4.charAt(0) + "+" + row5.charAt(0) + "+" + row6.charAt(0) + "+" + row7.charAt(0) + "+" + row8.charAt(0) + "+" + row9.charAt(0)));
 var vertTwo = (eval(row1.charAt(2) + "+" + row2.charAt(2) + "+" + row3.charAt(2) + "+" + row4.charAt(2) + "+" + row5.charAt(2) + "+" + row6.charAt(2) + "+" + row7.charAt(2) + "+" + row8.charAt(2) + "+" + row9.charAt(2)));
 var vertThree = (eval(row1.charAt(4) + "+" + row2.charAt(4) + "+" + row3.charAt(4) + "+" + row4.charAt(4) + "+" + row5.charAt(4) + "+" + row6.charAt(4) + "+" + row7.charAt(4) + "+" + row8.charAt(4) + "+" + row9.charAt(4)));
@@ -64,15 +70,29 @@ var vertSix = (eval(row1.charAt(10) + "+" + row2.charAt(10) + "+" + row3.charAt(
 var vertSeven = (eval(row1.charAt(12) + "+" + row2.charAt(12) + "+" + row3.charAt(12) + "+" + row4.charAt(12) + "+" + row5.charAt(12) + "+" + row6.charAt(12) + "+" + row7.charAt(12) + "+" + row8.charAt(12) + "+" + row9.charAt(12)));
 var vertEight = (eval(row1.charAt(14) + "+" + row2.charAt(14) + "+" + row3.charAt(14) + "+" + row4.charAt(14) + "+" + row5.charAt(14) + "+" + row6.charAt(14) + "+" + row7.charAt(14) + "+" + row8.charAt(14) + "+" + row9.charAt(14)));
 var vertNine = (eval(row1.charAt(16) + "+" + row2.charAt(16) + "+" + row3.charAt(16) + "+" + row4.charAt(16) + "+" + row5.charAt(16) + "+" + row6.charAt(16) + "+" + row7.charAt(16) + "+" + row8.charAt(16) + "+" + row9.charAt(16)));
-return vertOne + " " + vertTwo + " " + vertThree + " " + vertFour + " " + vertFive + " " + vertSix;
+var vertRow = vertOne + " " + vertTwo + " " + vertThree + " " + vertFour + " " + vertFive + " " + vertSix + " " + vertSeven + " " + vertEight + " " + vertNine;
+// convert vertRow to an array of integers
+var change = vertRow.split(" ");
+var vertRowIntegers = [];
+var result = change.map(function (z) { 
+	vertRowIntegers.push(parseInt(z, 10)); 
+});
+// finally, evaluate the expression. numbers 1-9 will = 45
+for(var i = 0; i < vertRowIntegers.length; i++){
+	if(vertRowIntegers[i] != 45) {
+		return false;
+	} else {
+		return true
+	}	
+}
 
 }
-validSolution([[1,8,7,4,5,6,3,2,9], 
-	[1,9,7,4,5,6,3,2,8], 
-	[1,2,3,4,6,5,7,8,9], 
-	[1,2,7,4,6,5,3,8,9], 
-	[1,2,3,5,4,6,7,8,9], 
-	[1,2,4,3,5,6,7,8,9],
-	[1,3,2,4,5,6,7,8,9],
-	[2,1,3,4,5,6,7,8,9],
-	[1,2,3,4,5,6,8,7,9]]);
+validSolution([[6,4,9,5,2,7,8,1,3],
+	[3,2,5,8,4,1,7,6,9],
+	[8,7,1,6,9,3,5,2,4],
+	[4,5,3,1,8,6,2,9,7],
+	[9,1,6,4,7,2,3,8,5],
+	[2,8,7,9,3,5,6,4,1],
+	[1,3,8,7,6,4,9,5,2],
+	[7,6,4,2,5,9,1,3,8],
+	[5,9,2,3,1,8,4,7,6]]);
